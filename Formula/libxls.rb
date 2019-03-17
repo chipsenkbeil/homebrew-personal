25b5e3f762eadb5f6fe42f9043c7458cf27d304b
Chip Senkbeil <chip.senkbeil@gmail.com>
2019-03-16 21:24:48 -0500

    Added libxls formula

---
+++ Formula/libxls.rb
@@ -0,0 +1,19 @@
+class LibXls < Formula
+  desc "Library to support basic XLS imports"
+  homepage "https://github.com/libxls/libxls"
+  url "https://github.com/libxls/libxls/releases/download/v1.5.0/libxls-1.5.0.tar.gz"
+  sha256 "c53a55187aeb0545e6fb5cfc89ad9521af80b1afc7eff407ac94c1bd59a64202"
+  head "https://github.com/libxls/libxls.git", :branch => "master"
+
+  def install
+    system "./bootstrap"
+    system "./configure"
+    system "make", "prefix=#{prefix}"
+    system "make", "prefix=#{prefix}", "install"
+  end
+
+  test do
+    output = pipe_output("#{bin}/xls2csv --version 2>/dev/null")
+    assert_equal "2", output.lines.last.chomp
+  end
+end
