1aecfd81e2a245bd70dba7cae16624d085008438
Chip Senkbeil <chipsenkbeil@fb.com>
2019-03-24 00:30:38 -0500

    Updated to new notmuch version

--- Formula/notmuch.rb
+++ Formula/notmuch.rb
@@ -1,17 +1,9 @@
 class Notmuch < Formula
   desc "Thread-based email index, search, and tagging"
   homepage "https://notmuchmail.org"
-  url "https://notmuchmail.org/releases/notmuch-0.27.tar.gz"
-  sha256 "40d3192f8f130f227b511fc80be86310c7f60ccb6d043b563f201fa505de0876"
+  url "https://notmuchmail.org/releases/LATEST-notmuch-0.28.3.tar.gz"
+  sha256 "4e212d8b4ae30da04edb05d836dcdb569488ff6760706cecb882488eb1710eec"
   head "git://notmuchmail.org/git/notmuch"
-
-  bottle do
-    cellar :any
-    rebuild 2
-    sha256 "85ed63058e3f8a62375e9df08963fdcf06423cfa8e8e42b1b43952b93d5828b6" => :mojave
-    sha256 "241b8649b30055854bfea7f0540099895e7915402c5b278ef5db838545f006a7" => :high_sierra
-    sha256 "bc53f8af373350cb3a993ca381eaaf1245d70874e24611c9a7e95568e96f922a" => :sierra
-  end
 
   # Re-enable installing with python3 bindings, which seems to be
   # missing from the stock repo formula
